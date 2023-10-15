46479d9be33e1e7aaf75e1f4bd8cdce86faea9f0
Javier Bergasa Palacios <64484123+jagitdev@users.noreply.github.com>
2023-10-16 00:58:44 +0200

    dos cambios de error

--- demo/src/main/java/com/example/demo/Controller/HabitacionControlador.java
+++ demo/src/main/java/com/example/demo/Controller/HabitacionControlador.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v2")
+@RequestMapping("/api/v1")
 public class HabitacionControlador {
     @Autowired
     private HabitacionServicioImplementacion habitacionServicio;
